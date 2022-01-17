0c828516cabd890445892e146b0698a5f62fed98
JoshuaDuma <Joshua@dumahomes.com>
2022-01-17 01:16:35 -0500

    Fixed path
    Signed-off-by: JoshuaDuma <Joshua@dumahomes.com>

--- docker_nodejs/app/server.js
+++ docker_nodejs/app/server.js
@@ -5,11 +5,13 @@ const app = express();
 // Constants
 const PORT = process.env.PORT;
 const SERVER_ID = process.env.SERVER_ID;
+var filePath = "./website/build/index.html"
+var resolvedPath = path.resolve(filePath);
 
 app.use(express.static(path.join(__dirname, 'website/build')));
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'website', 'build', 'index.html'), function(err) {
+  res.sendFile(resolvedPath, function(err) {
     if (err) {
       res.status(500).send(err)
     }
