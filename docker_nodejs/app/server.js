93b11c478f4259d7d36b26b84afa09a3d3776021
JoshuaDuma <Joshua@dumahomes.com>
2022-01-17 00:29:10 -0500

    testing with actual hello world
    Signed-off-by: JoshuaDuma <Joshua@dumahomes.com>

--- docker_nodejs/app/server.js
+++ docker_nodejs/app/server.js
@@ -8,10 +8,8 @@ const SERVER_ID = process.env.SERVER_ID;
 
 app.use(express.static(path.join(__dirname, 'website/build')));
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+app.get('/*', function (req, res) {
+  res.send('Hello World!');
 });
 
 app.listen(PORT, function () {
