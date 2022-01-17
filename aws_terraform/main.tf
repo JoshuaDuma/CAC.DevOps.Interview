c9f568b166125b14bed35f3e1d798e688f44602e
JoshuaDuma <Joshua@dumahomes.com>
2022-01-17 00:13:59 -0500

    Fixed a mistake in the user_data command
    Signed-off-by: JoshuaDuma <Joshua@dumahomes.com>

--- aws_terraform/main.tf
+++ aws_terraform/main.tf
@@ -32,10 +32,14 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.medium"
 
   monitoring = true
+  resource "aws_key_pair" "deployer" {
+    key_name   = "deployer-key"
+    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD3F6tyPEFEzV0LX3X8BsXdMsQz1x2cEikKDEY0aIj41qgxMCP/iteneqXSIFZBp5vizPvaoIR3Um9xK7PGoW8giupGn+EPuxIA4cDM4vzOqOkiMPhz5XK0whEjkVzTo4+S0puvDZuwIsdiW9mxhJc7tgBNL0cYlWSYVkz4G/fslNfRPW5mYAM49f4fhtxPb5ok4Q2Lg9dPKVHO/Bgeu5woMc7RY0p1ej6D4CKFE6lymSDJpW0YHX/wqE9+cfEauh7xZcG0q9t2ta6F6fmX0agvpFyZo8aFbXeUBr7osSCJNgvavWbM/06niWrOvYX2xwWdhXmXSrbX8ZbabVohBK41 email@example.com"
+  }
 
   tags = {
     Name = "CAC_DevOps_Interview"
@@ -50,12 +54,7 @@ resource "aws_instance" "app_server" {
     sudo curl -L https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
     git clone https://github.com/JoshuaDuma/CAC.DevOps.Interview.git
-<<<<<<< HEAD
     cd CAC.DevOps.Interview/docker_nodejs
     sudo docker-compose up --build
-=======
-    cd load_balanced_nodejs_app/app
-    docker build -t app .
->>>>>>> b1dd53eee03e0f27aa08c6521922c4c22e2c20b4
   EOL
 }
