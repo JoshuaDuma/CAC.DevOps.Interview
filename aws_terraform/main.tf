e1bf955f627cb5f76399195c936960a4d3b81a2d
JoshuaDuma <Joshua@dumahomes.com>
2022-01-17 00:03:21 -0500

    Fixed some more issues.
    Signed-off-by: JoshuaDuma <Joshua@dumahomes.com>

--- aws_terraform/main.tf
+++ aws_terraform/main.tf
@@ -14,11 +14,32 @@ provider "aws" {
   region  = "us-west-2"
 }
 
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.medium"
 
   monitoring = true
+  resource "aws_key_pair" "deployer" {
+    key_name   = "deployer-key"
+    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD3F6tyPEFEzV0LX3X8BsXdMsQz1x2cEikKDEY0aIj41qgxMCP/iteneqXSIFZBp5vizPvaoIR3Um9xK7PGoW8giupGn+EPuxIA4cDM4vzOqOkiMPhz5XK0whEjkVzTo4+S0puvDZuwIsdiW9mxhJc7tgBNL0cYlWSYVkz4G/fslNfRPW5mYAM49f4fhtxPb5ok4Q2Lg9dPKVHO/Bgeu5woMc7RY0p1ej6D4CKFE6lymSDJpW0YHX/wqE9+cfEauh7xZcG0q9t2ta6F6fmX0agvpFyZo8aFbXeUBr7osSCJNgvavWbM/06niWrOvYX2xwWdhXmXSrbX8ZbabVohBK41 email@example.com"
+  }
 
   tags = {
     Name = "CAC_DevOps_Interview"
@@ -27,14 +48,13 @@ resource "aws_instance" "app_server" {
   user_data = <<-EOL
     #!/bin/bash
     set -ex
-    sudo yum update -y
-    sudo amazon-linux-extras install docker -y
+    sudo apt update
+    sudo apt install -y docker.io
     sudo service docker start
-    sudo usermod -a -G docker ec2-user
-    sudo curl -L https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+    sudo curl -L https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
     git clone https://github.com/JoshuaDuma/CAC.DevOps.Interview.git
-    cd load_balanced_nodejs_app/app
-    docker build -t app .
+    cd CAC.DevOps.Interview/docker_nodejs
+    sudo docker-compose up --build
   EOL
 }
