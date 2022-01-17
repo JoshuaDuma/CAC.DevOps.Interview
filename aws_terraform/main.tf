63628ea87998c8223ee60e6e47af3b10d378d8c6
JoshuaDuma <Joshua@dumahomes.com>
2022-01-17 01:15:01 -0500

    Fixed security group issue.
    Signed-off-by: JoshuaDuma <Joshua@dumahomes.com>

--- aws_terraform/main.tf
+++ aws_terraform/main.tf
@@ -11,7 +11,7 @@ terraform {
 
 provider "aws" {
   profile = "default"
-  region  = "us-west-2"
+  region  = "us-east-1"
 }
 
 
@@ -36,10 +36,10 @@ resource "aws_instance" "app_server" {
   instance_type = "t2.medium"
 
   monitoring = true
-  resource "aws_key_pair" "deployer" {
-    key_name   = "deployer-key"
-    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD3F6tyPEFEzV0LX3X8BsXdMsQz1x2cEikKDEY0aIj41qgxMCP/iteneqXSIFZBp5vizPvaoIR3Um9xK7PGoW8giupGn+EPuxIA4cDM4vzOqOkiMPhz5XK0whEjkVzTo4+S0puvDZuwIsdiW9mxhJc7tgBNL0cYlWSYVkz4G/fslNfRPW5mYAM49f4fhtxPb5ok4Q2Lg9dPKVHO/Bgeu5woMc7RY0p1ej6D4CKFE6lymSDJpW0YHX/wqE9+cfEauh7xZcG0q9t2ta6F6fmX0agvpFyZo8aFbXeUBr7osSCJNgvavWbM/06niWrOvYX2xwWdhXmXSrbX8ZbabVohBK41 email@example.com"
-  }
+
+  security_groups = [
+    "Awesome Security Group"
+  ]
 
   tags = {
     Name = "CAC_DevOps_Interview"
